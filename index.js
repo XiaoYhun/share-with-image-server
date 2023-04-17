963a14531587871e20f828577ef649a19be49e22
Diep Pham <xiaoyhun@gmail.com>
2023-04-17 14:51:04 +0700

    revert

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
   const metaImage = req.query.imageurl;
-  const redirectUrl = req.query.redirectUrl;
+  const redirectUrl = req.query.redirecturl;
   const filePath = path.resolve("./index.html");
   if (metaImage) {
     filePath.replace("@META_IMAGE", metaImage);
   }
   res.sendFile(filePath);
   if (redirectUrl) {
-    res.redirect("https://kyberswap.com/");
+    res.redirect(redirectUrl);
   }
 });
 
